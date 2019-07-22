d9b66b5e3bf618bb59e9da76f9ea8fef63dc29d5
HarshalKulkarni <HarshalKulkarni@DESKTOP-0IMA24S.in.ibm.com>
2019-07-22 20:50:53 +0530

    PropertySearch POM + PropertySearch Test class Code - tested and verified.
    Signed-off-by: HarshalKulkarni <HarshalKulkarni@DESKTOP-0IMA24S.in.ibm.com>

---
+++ final-framework-testng/src/com/training/pom/PropertysearchPOM.java
@@ -0,0 +1,92 @@
+package com.training.pom;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PropertysearchPOM {
+
+      private WebDriver driver; 
+
+
+	public PropertysearchPOM(WebDriver driver) {
+		this.driver = driver; 
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath="//ul[@id='responsive']//a[contains(text(),'New Launch')]")
+	private WebElement newLaunch;
+	public void clicknewlaunch() {
+		newLaunch.click();
+	}
+	public String getfindyourhometitle() {
+		return driver.getTitle();
+	}
+
+	@FindBy(id="keyword_search")
+	private WebElement addfield;
+
+	public void enteraddress(String address) {
+		this.addfield.sendKeys(address); 
+		driver.findElement(By.xpath("//h4[@class='search-title']")).click();		
+	}
+	
+	@FindBy(xpath="//a[@class='chosen-single chosen-default']")
+	private WebElement propertytypedrop;
+	
+	@FindBy(xpath = "//*[@id='_property_type_chosen']/div/ul/li[4]")
+	private WebElement propertytype;
+
+	public void selectpropertytypePLOT() throws InterruptedException {
+		propertytypedrop.click();
+		Thread.sleep(2000);
+		WebDriverWait wait = new WebDriverWait(driver,100);
+	    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='_property_type_chosen']/div/ul/li[4]")));
+	    propertytype.click();
+	}
+	
+	@FindBy(xpath="//*[@id='realteo-search-form']/div[2]/div[2]/div/a/span")
+	private WebElement anyregion;	
+	
+	
+	@FindBy(xpath = "//*[@id='realteo-search-form']/div[2]/div[2]/div/div/ul/li[2]")
+	private WebElement selectregion;
+	
+	public void selectregionCentralBengalore() throws InterruptedException {
+		anyregion.click();	
+		Thread.sleep(2000);
+		selectregion.click();
+	}
+	
+	@FindBy(xpath="//button[@class='button fullwidth']")
+	private WebElement searchbutton;
+	
+	public void searchbuttonclick() throws InterruptedException {
+		searchbutton.click();
+	}
+	
+	@FindBy(xpath="//h3[text( )=  'Recent Posts']")
+	private WebElement Searchresultpage;
+
+	
+	public String searchresultspagecheck() {
+		return Searchresultpage.getText();
+	}
+	
+	
+	
+
+
+
+}
---
+++ final-framework-testng/tests/com/training/sanity/tests/PropertysearchPOMTest.java
@@ -0,0 +1,85 @@
+package com.training.sanity.tests;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.training.generics.ScreenShot;
+import com.training.pom.PropertysearchPOM;
+import com.training.utility.DriverFactory;
+import com.training.utility.DriverNames;
+
+public class PropertysearchPOMTest {
+
+	private WebDriver driver;
+	private String baseUrl;
+	private PropertysearchPOM propertysearchPOM;
+	private static Properties properties;
+	private ScreenShot screenShot;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws IOException {
+		properties = new Properties();
+		FileInputStream inStream = new FileInputStream("./resources/others.properties");
+		properties.load(inStream);
+	}
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		driver = DriverFactory.getDriver(DriverNames.CHROME);
+		propertysearchPOM = new PropertysearchPOM(driver);
+		baseUrl = properties.getProperty("baseURL");
+		screenShot = new ScreenShot(driver);
+		driver.get(baseUrl);
+	}
+
+	@AfterMethod
+	public void tearDown() throws Exception {
+		Thread.sleep(5000);
+		driver.quit();
+	}
+
+	@Test
+	public void propertysearchtest() throws InterruptedException {
+
+		propertysearchPOM.clicknewlaunch();
+		System.out.println(propertysearchPOM.getfindyourhometitle());
+
+		try {
+			Assert.assertEquals(propertysearchPOM.getfindyourhometitle(), "New Launch – Real Estate");
+			System.out.println("User is on Property Search as expected");
+
+		} catch (Exception e) {
+			System.out.println("user landed on wrong page...please check with developer on this..");
+			screenShot.captureScreenShot("Incorrect_Home_title_page");
+		}
+		propertysearchPOM.enteraddress("Electronic City, Bengaluru, Karnataka, India");
+		propertysearchPOM.selectpropertytypePLOT();
+		propertysearchPOM.selectregionCentralBengalore();
+		propertysearchPOM.searchbuttonclick();
+		System.out.println(propertysearchPOM.searchresultspagecheck());
+
+		try {
+			Assert.assertEquals(propertysearchPOM.searchresultspagecheck(), "Recent Posts");
+			System.out.println("User is on Property Search results page after inputs as expected");
+
+
+		} catch (Exception e) {
+			System.out.println("user landed on wrong page...please check with developer on this..");
+			screenShot.captureScreenShot("Incorrect_search_results_title_page");
+		}
+
+	}
+}
